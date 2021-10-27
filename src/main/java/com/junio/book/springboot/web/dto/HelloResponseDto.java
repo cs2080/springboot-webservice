ea5ffd7e5b807b0f3ad59ee676654f8b376847e2
junio <junio5188@gmail.com>
2021-10-27 14:21:42 +0900

    fixed untracked files

---
+++ src/main/java/com/junio/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.junio.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
