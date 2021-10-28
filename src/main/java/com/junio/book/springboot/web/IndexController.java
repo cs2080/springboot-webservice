4256c7193f7722768a60243be0bdd7e07cadf90c
junio <junio5188@gmail.com>
2021-10-28 16:59:35 +0900

    7장완료

--- src/main/java/com/junio/book/springboot/web/IndexController.java
+++ src/main/java/com/junio/book/springboot/web/IndexController.java
@@ -1,23 +1,31 @@
 package com.junio.book.springboot.web;
 
+import com.junio.book.springboot.config.auth.LoginUser;
+import com.junio.book.springboot.config.auth.dto.SessionUser;
 import com.junio.book.springboot.service.posts.PostsService;
 import com.junio.book.springboot.web.dto.PostsResponseDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
